83bf908c4d83cbdcfe3d4e1fd33b3231f808bdf2
KANDUKURI <gayathri-ramana.kandukuri@capgemini.com>
2021-10-09 11:19:04 +0530

    added new var

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        public int a; 
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Program p = new Program();
+            p.hello();
+        }
+
+        public void hello()
+        {
+            Console.WriteLine("Cloud azure training");
+        }
+    }
+}
